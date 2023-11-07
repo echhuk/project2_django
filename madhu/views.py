466a3d73329d25864599f07b618849758b69d7dc
echhuk <echhuksharma@gmail.com>
2023-11-08 00:31:55 +0530

    first commit

---
+++ madhu/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def navneet(request):
+    return HttpResponse('hi this is navneet')
+
+def sona(request):
+    return HttpResponse('sona hee sona')
+
